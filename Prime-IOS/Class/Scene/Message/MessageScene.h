8c97cfdda0aba66bb1f104c2c03686cbb8f74984
gilbert <haiob88@163.com>
2015-04-14 17:53:58 +0800

    init scene

---
+++ Prime-IOS/Class/Scene/Message/MessageScene.h
@@ -0,0 +1,13 @@
+//
+//  MessageScene.h
+//  Prime-IOS
+//
+//  Created by gypsii001 on 15/4/14.
+//  Copyright (c) 2015年 EasyIOS. All rights reserved.
+//
+
+#import "Scene.h"
+
+@interface MessageScene : Scene
+
+@end
